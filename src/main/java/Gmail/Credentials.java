7052c685ae53f97f70ce659a45b9cefb5aa17d4d
your_github_username <your_github_email>
2019-11-05 16:02:50 -0500

    Initial commit

---
+++ src/main/java/Gmail/Credentials.java
@@ -0,0 +1,13 @@
+package Gmail;
+
+public class Credentials {
+private String userName = "Cybertek.Batch@gmail.com";
+private String password ="Batch12BestBatch";
+public String getUserName(){
+    return userName;
+}
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/Gmail/SenEmails.java
@@ -0,0 +1,36 @@
+package Gmail;
+
+import org.apache.commons.mail.SimpleEmail;
+public class SenEmails {
+    /*
+         first Step:
+             go to  :
+                  https://myaccount.google.com/lesssecureapps?utm_source=google-account&utm_medium=web
+                     Make sure that your google account allows third part app
+      */
+    public static void main(String[] args) {
+        String userName = "Cybertek.Batch@gmail.com",
+                passWord = "Batch12BestBatch",
+                Receiver = "Cybertek.Batch@gmail.com",
+                MSG ="Hello Batch 12, How are you all doing today",
+                Subject ="Batch12";
+        sendEmails( userName, passWord, Receiver, MSG , Subject);
+        System.out.println("Send successfully");
+    }
+    public static void sendEmails(String username, String password, String Receiver, String MSG, String Subject){
+        SimpleEmail email=new SimpleEmail();
+        email.setHostName("smtp.googlemail.com");
+        email.setSmtpPort(465);
+        email.setAuthentication(username, password);
+        email.setSSLOnConnect(true);
+        email.setSubject(Subject);
+        try {
+            email.setFrom(username);
+            email.setMsg(MSG);
+            email.addTo(Receiver);
+            email.send();
+            Thread.sleep(3000);
+        } catch (Exception e) {   }
+    }
+}
+
